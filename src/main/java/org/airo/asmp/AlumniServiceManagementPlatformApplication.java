3df432d7bf67cd6a6e04eb21beeb24314b92cf6c
useless <798370740@qq.com>
2025-05-04 02:16:39 +0800

    添加 Swagger OpenAPI 文档支持

--- src/main/java/org/airo/asmp/AlumniServiceManagementPlatformApplication.java
+++ src/main/java/org/airo/asmp/AlumniServiceManagementPlatformApplication.java
@@ -3,8 +3,11 @@ package org.airo.asmp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "校友服务管理平台 API", version = "1.0", description = "API 文档"))
 @EntityScan(basePackages = "org.airo.asmp.model")
 public class AlumniServiceManagementPlatformApplication {
 
