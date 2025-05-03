cda1e533ef70b8b1b71a4764aefd65b0dc8f1330
useless <798370740@qq.com>
2025-05-04 02:47:24 +0800

    描述您的更改

--- src/test/java/org/airo/asmp/AlumniServiceManagementPlatformApplicationTests.java
+++ src/test/java/org/airo/asmp/AlumniServiceManagementPlatformApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AlumniServiceManagementPlatformApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
