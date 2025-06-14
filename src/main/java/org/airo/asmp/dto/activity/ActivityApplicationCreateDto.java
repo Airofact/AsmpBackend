dc45dbd542d1d17c321a423b3fc39e97fb2edbd4
Airo <bigod857861926@163.com>
2025-06-14 17:59:36 +0800

    docker相关的：数据库初始化脚本

--- src/main/java/org/airo/asmp/dto/activity/ActivityApplicationCreateDto.java
+++ src/main/java/org/airo/asmp/dto/activity/ActivityApplicationCreateDto.java
@@ -1,9 +1,11 @@
 package org.airo.asmp.dto.activity;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
 /**
  * DTO for creating ActivityApplication
  * activityId will be provided via path parameter
  */
-public record ActivityApplicationCreateDto(UUID alumniId){}
+public record ActivityApplicationCreateDto(@NotNull UUID alumniId){}
